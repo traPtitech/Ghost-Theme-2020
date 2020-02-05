b7523c3d67a28963ed2ce0a77233e8f129b9b429
sapphi-red <green@sapphi.red>
2020-02-05 23:12:42 +0900

    fix running before loaded

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,63 +5,64 @@ import { faTwitter, faFacebookF } from '@fortawesome/free-brands-svg-icons';
 
 import '../css/style.scss';
 
-// font awesome
 library.add(faHome, faShareAlt, faEnvelope, faTwitter, faFacebookF);
-if (document.readyState !== 'loading') {
+
+const onLoad = () => {
+	// font awesome
 	dom.i2svg();
-} else {
-	window.addEventListener("DOMContentLoaded", () => {
-		dom.i2svg();
-	})
-}
 
-// fill copyright year
-window.addEventListener("load", () => {
+	// fill copyright year
 	document.querySelector(".copy span").textContent = new Date().getFullYear();
-});
 
-const $hyakkiyagyo = document.querySelector("#hyakkiyagyo")
-if ($hyakkiyagyo) {
-	const hyakkiyagyoOrigin = "https://sysad.trap.show"
+	const $hyakkiyagyo = document.querySelector("#hyakkiyagyo")
+	if ($hyakkiyagyo) {
+		const hyakkiyagyoOrigin = "https://sysad.trap.show"
 
-	// adjust iframe size
-	window.addEventListener("message", (event) => {
-		if(event.origin === hyakkiyagyoOrigin){
-			$hyakkiyagyo.style.height = event.data + "px";
-		}
-	});
+		// adjust iframe size
+		window.addEventListener("message", (event) => {
+			if(event.origin === hyakkiyagyoOrigin){
+				$hyakkiyagyo.style.height = event.data + "px";
+			}
+		});
 
-	// send css path to hyakkiyagyo
-	$hyakkiyagyo.addEventListener("load", () => {
-		$hyakkiyagyo.contentWindow.postMessage(
-			document.querySelector("link[rel=stylesheet][href*=\\/app]").href,
-			hyakkiyagyoOrigin
-		)
-	})
-}
-
-// add link to image
-document.querySelectorAll("article p > img, article .kg-image-card > img, article .kg-gallery-image > img").forEach((img) => {
-	const link = document.createElement("a");
-	link.target = "_blank";
-	link.href = img.src;
-	link.className = "orig-link";
-	img.replaceWith(link);
-	link.appendChild(img);
-});
-
-// web share
-const $share = document.querySelector("#web-share")
-if ($share) {
-	if (navigator.share) {
-		$share.classList.remove("disabled")
-		$share.addEventListener("click", () => {
-			navigator.share({
-				title: document.title,
-				url: location.href,
-			}).catch(e => {
-				console.error("failed to share", e);
-			})
+		// send css path to hyakkiyagyo
+		$hyakkiyagyo.addEventListener("load", () => {
+			$hyakkiyagyo.contentWindow.postMessage(
+				document.querySelector("link[rel=stylesheet][href*=\\/app]").href,
+				hyakkiyagyoOrigin
+			)
 		})
 	}
+
+	// add link to image
+	document.querySelectorAll("article p > img, article .kg-image-card > img, article .kg-gallery-image > img").forEach((img) => {
+		const link = document.createElement("a");
+		link.target = "_blank";
+		link.href = img.src;
+		link.className = "orig-link";
+		img.replaceWith(link);
+		link.appendChild(img);
+	});
+
+	// web share
+	const $share = document.querySelector("#web-share")
+	if ($share) {
+		if (navigator.share) {
+			$share.classList.remove("disabled")
+			$share.addEventListener("click", () => {
+				navigator.share({
+					title: document.title,
+					url: location.href,
+				}).catch(e => {
+					console.error("failed to share", e);
+				})
+			})
+		}
+	}
+}
+
+if (document.readyState !== 'loading') {
+	onLoad()
+} else {
+	window.addEventListener("DOMContentLoaded", onLoad)
 }
