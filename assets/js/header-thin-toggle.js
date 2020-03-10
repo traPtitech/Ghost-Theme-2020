100c35b360744bc86205f34c9f9ac078c4090280
sapphi-red <green@sapphi.red>
2020-03-10 22:22:06 +0900

    fix 50fb63e refs #10

--- assets/js/header-thin-toggle.js
+++ assets/js/header-thin-toggle.js
@@ -1,22 +1,19 @@
+import throttle from 'lodash.throttle';
+
 const convertRemToPx = rem => {
   const fontSize = getComputedStyle(document.documentElement).fontSize;
   return rem * parseFloat(fontSize);
 }
 
 export const setupHeaderThinToggle = () => {
-  const topPx = convertRemToPx(6)
-  const interval = 100;
-  const $header = document.getElementsByTagName('header')[0]
-  let lastTime = Date.now() - interval
+  const topPx = convertRemToPx(6);
+  const $header = document.getElementsByTagName('header')[0];
 
-  window.addEventListener('scroll', () => {
-    if (Date.now() < (lastTime + interval)) return
-    lastTime = Date.now();
-
+  window.addEventListener('scroll', throttle(() => {
     if (document.body.scrollTop > topPx || document.documentElement.scrollTop > topPx) {
       $header.classList.add('thin')
     } else {
       $header.classList.remove('thin')
     }
-  });
+  }, 100, { trailing: true }));
 };
