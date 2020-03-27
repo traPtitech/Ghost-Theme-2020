594027c7bd2e6a9cb55bec15591a48663b0c8807
sapphi-red <green@sapphi.red>
2020-03-27 15:48:20 +0900

    ヘッダーの高さが振動する refs #49

--- assets/js/header-thin-toggle.js
+++ assets/js/header-thin-toggle.js
@@ -7,12 +7,13 @@ const convertRemToPx = rem => {
 
 export const setupHeaderThinToggle = () => {
   const topPx = convertRemToPx(6);
+  const thinTopPx = convertRemToPx(3.5);
   const $header = document.getElementsByTagName('header')[0];
 
   window.addEventListener('scroll', throttle(() => {
     if (document.body.scrollTop > topPx || document.documentElement.scrollTop > topPx) {
       $header.classList.add('thin')
-    } else {
+    } else if (document.body.scrollTop < thinTopPx && document.documentElement.scrollTop < thinTopPx) {
       $header.classList.remove('thin')
     }
   }, 100, { trailing: true }));
