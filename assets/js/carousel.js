c2b22bc049554ae1770c763c268ecd2443a67ef8
sapphi-red <green@sapphi.red>
2020-12-07 17:25:02 +0900

    optimize swiper init refs #54

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,25 +1,36 @@
 import Swiper, {Navigation, Pagination, Lazy} from "swiper";
+import { debounce } from "throttle-debounce";
 
-export const setupCarousel = () => {
-	const $featured = document.querySelector('#featured')
-	if (!$featured) return;
+const BREAKPOINT = 840
+const MAX_WIDTH = 1280
 
-	Swiper.use([Navigation, Pagination, Lazy]);
+const smartphoneOption = {
+	slidesPerView: 1,
+	spaceBetween: 0
+}
+
+const pcOption = {
+	slidesPerView: 2,
+	spaceBetween: 10,
+	watchSlidesProgress: true,
+	watchSlidesVisibility: true
+}
+
+const initSwiper = oldSwiper => {
+	if (oldSwiper) {
+		oldSwiper.destroy()
+	}
+
+	const options =
+		window.innerWidth >= BREAKPOINT
+			? pcOption
+			: smartphoneOption
 
 	return new Swiper($featured, {
 		loop: true,
-		slidesPerView: 1,
-		spaceBetween: 0,
 		centeredSlides : true,
 		preloadImages: false,
-		breakpoints: {
-			840: {
-				slidesPerView: 2,
-				spaceBetween: 10,
-				watchSlidesProgress: true,
-				watchSlidesVisibility: true
-			}
-		},
+		width: Math.min(window.innerWidth, MAX_WIDTH),
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev'
@@ -31,6 +42,19 @@ export const setupCarousel = () => {
 		},
 		lazy: {
 			loadOnTransitionStart: true
-		}
+		},
+		...options
 	});
+}
+
+export const setupCarousel = () => {
+	const $featured = document.querySelector('#featured')
+	if (!$featured) return;
+
+	Swiper.use([Navigation, Pagination, Lazy]);
+
+	let swiper = initSwiper();
+	window.addEventListener('resize', debounce(100, () => {
+		swiper = initSwiper(swiper);
+	}))
 };
